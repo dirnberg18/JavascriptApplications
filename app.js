6b7e49934c908980f3f3d39272f75121a5d34f7a
Pekka Alaluukas <alaluuk@oamk.fi>
2020-10-28 09:56:32 +0200

    book works

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
+var helmet = require("helmet");
+var app = express();
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/book');
+
+
+app.use(cors());  
+app.use(helmet());
+
+
 
 var app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
