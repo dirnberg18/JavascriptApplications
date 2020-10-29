2de1a4bab1beeec520e934fd18f73d615b681aa0
Pekka Alaluukas <alaluuk@oamk.fi>
2020-10-29 09:41:51 +0200

    ready for heroku

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  user: 'netuser',
-  host: 'localhost',
-  database: 'netdb',
-  password: 'netpass',
+  user: 'miilxykauqjtis',
+  host: 'ec2-54-246-85-151.eu-west-1.compute.amazonaws.com',
+  database: 'd6p25ffbcdifin',
+  password: '5e9db7d91ded0356867c3b585e45be50475128cc1a9b59ab49e9f5f4bc4690bd',
   port: 5432,
 })
 module.exports = connection;
